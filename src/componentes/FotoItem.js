5a6f7d08001fc5c09a8fa77ca0d25db03c0e0492
Juan Carlos <juantavares2006@hotmail.com>
2018-05-21 23:36:52 -0300

    Definindo como é o FotoItem default

--- src/componentes/FotoItem.js
+++ src/componentes/FotoItem.js
@@ -24,6 +24,13 @@ class FotoHeader extends Component {
 export default class FotoItem extends Component {
 
     render() {
-        return;
+        return (
+            <div className="foto">
+                <FotoHeader />
+                <img alt="foto" className="foto-src" src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2" />
+                <FotoInfo />
+                <FotoAtualizacoes />
+            </div>
+        );
     }
 }
