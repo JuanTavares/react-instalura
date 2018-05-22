4e812c003e746245e78a235be0ca80b850069803
Juan Carlos <juantavares2006@hotmail.com>
2018-05-21 23:36:06 -0300

    Criando e definindo corpo do componente FotoItem

---
+++ src/componentes/FotoItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class FotoAtualizacoes extends Component {
+
+    render() {
+        return;
+    }
+}
+
+class FotoInfo extends Component {
+
+    render() {
+        return;
+    }
+}
+
+class FotoHeader extends Component {
+
+    render() {
+        return;
+    }
+}
+
+export default class FotoItem extends Component {
+
+    render() {
+        return;
+    }
+}
