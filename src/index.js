e6a317e384a835e84d9d5b6481c98458051de18b
Juan Carlos <juantavares2006@hotmail.com>
2018-05-21 23:47:02 -0300

    Aplicando css na página

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/timeline.css';
+import './css/reset.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
