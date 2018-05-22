3747511daa52b08d0b02475140f17da86d7d2b9b
Juan Carlos <juantavares2006@hotmail.com>
2018-05-21 23:26:15 -0300

    Refatorando app.js com a aparência desejada
    Criando ainda os componentes em branco

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
 
 class App extends Component {
   render() {
     return (
+      <div id="root">
+        <div className="main">
+          <Header />
+          <Timeline />
+        </div>
+      </div>
     );
   }
 }
