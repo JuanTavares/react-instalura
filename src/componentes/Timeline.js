1363681ad313b151d95c37db612606bc146d3997
Juan Carlos <juantavares2006@hotmail.com>
2018-05-22 21:12:34 -0300

    Configurando componentDidMount

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -3,6 +3,18 @@ import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
 
+    constructor() {
+        super();
+        this.state = {lista:[]}
+    }
+    componentDidMount() {
+        fetch('https://instalura-api.herokuapp.com//api/public/fotos/vitor')
+        .then(Response => Response.json())
+        .then(fotos => {
+            this.setState({lista: fotos});
+        });
+    }
+
     render() {
         return (
             <div className="fotos container">
