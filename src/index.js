ae2626192c52d0cff0bcca289233b3d007ed3687
Juan Carlos <juantavares2006@hotmail.com>
2018-05-23 22:39:01 -0300

    Configurando rota

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './css/timeline.css';
 import './css/reset.css';
 import './css/login.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './componentes/Login';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route path="/timeline" component={App} />
+            </Switch>
+        </BrowserRouter>
+    )
+    , document.getElementById('root'));
 registerServiceWorker();
