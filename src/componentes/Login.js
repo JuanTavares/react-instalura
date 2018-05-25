74765bd9016cd8111559b97e2c39842bd9054d37
Juan Carlos <juantavares2006@hotmail.com>
2018-05-24 22:12:14 -0300

    Configurando requisição

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -4,17 +4,26 @@ export default class Login extends Component {
 
     constructor() {
         super();
-        this.state = {msg: ''};
+        this.state = { msg: '' };
     }
 
     envia(event) {
         event.preventDefault();
+
+        const resquetInfo = {
+            method: 'POST',
+            body: JSON.stringify({ login: this.login.value, senha: this.senha.value }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
         fetch('https://instalura-api.herokuapp.com//api/public/login', requestInfo)
-            .then(response=> {
-                if(response.ok) {
+            .then(response => {
+                if (response.ok) {
                     return response.text();
                 } else {
-                    this.setState({msg: 'Não foi possível fazer o login'})
+                    this.setState({ msg: 'Não foi possível fazer o login' })
                 }
             })
     }
