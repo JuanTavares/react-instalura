65ba9aadcb78d9cd568b81506f22275847bb9dc4
Juan Carlos <juantavares2006@hotmail.com>
2018-05-24 22:05:48 -0300

    Criando campo de msg

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -25,7 +25,7 @@ export default class Login extends Component {
             <div className="login-box">
                 <h1 className="header-logo">Instalura</h1>
                 <form onSubmit={this.envia}>
-
+                    <span>{this.state.msg}</span>
                     <input type="text" ref={(input) => this.login = input} />
                     <input type="password" ref={(input) => this.senha = input} />
                     <input type="submit" value="Login" />
