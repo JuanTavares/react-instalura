eb3afbb3cc81983d820c07b22bd482618aafd540
Juan Carlos <juantavares2006@hotmail.com>
2018-05-24 22:03:58 -0300

    Criando e definindo estado para msg de erro

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
 
+    constructor() {
+        super();
+        this.state = {msg: ''};
+    }
+
     envia() {
         event.preventDefault();
         fetch('https://instalura-api.herokuapp.com//api/public/login', requestInfo)
@@ -20,6 +25,7 @@ export default class Login extends Component {
             <div className="login-box">
                 <h1 className="header-logo">Instalura</h1>
                 <form onSubmit={this.envia}>
+
                     <input type="text" ref={(input) => this.login = input} />
                     <input type="password" ref={(input) => this.senha = input} />
                     <input type="submit" value="Login" />
