4e15b26f2ef34e955b5a22fd4275fd279f6e3e99
Juan Carlos <juantavares2006@hotmail.com>
2018-05-28 23:55:19 -0300

    Definindo logout

--- src/componentes/Logout.js
+++ src/componentes/Logout.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class Logout extends Component {
 
+    componentWillMount() {
+        localStorage.removeItem('auth-token');
+        this.props.history.push('/');
+    }
+
     render() {
         return null;
     }
